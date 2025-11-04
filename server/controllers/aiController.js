68603acbc7ced1cbff20b69207646903a78913f2
Shivansh Nigam <s2704nigam@gmail.com>
2025-11-05 03:05:18 +0530

    Add AI resume processing endpoints and model integration

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -1,5 +1,5 @@
 import ai from "../configs/ai";
-
+import Resume from "../models/Resume.js";
 
 // POST: /api/ai/enhance-pro-sum
 export const enhanceProfessionalSummary = async (req, res) => {
@@ -52,6 +52,85 @@ export const enhanceJobDescription = async (req, res) => {
     });
     const enhancedContent = response.choices[0].message.content;
     return res.status(200).json({ enhancedContent });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+};
+
+// POST: /api/ai/upload-resume
+export const uploadResume = async (req, res) => {
+  try {
+    const { resumeText, title } = req.body;
+    const userId = req.userId;
+    if (!resumeText) {
+      return res.status(400).json({ error: "Missing required field" });
+    }
+    const systemPrompt =
+      "You are an expert AI Agent to extract data from resume.";
+    const userPrompt = `Extract data from the following resume: ${resumeText}.
+    Provide data in the following JSON format with no additional text before or after:
+    {
+    professional_summary: {
+      type: String,
+      default: "",
+    },
+    skills: [{ type: String }],
+    personal_info: {
+      image: { type: String, default: "" },
+      full_name: { type: String, default: "" },
+      profession: { type: String, default: "" },
+      email: { type: String, default: "" },
+      phone: { type: String, default: "" },
+      location: { type: String, default: "" },
+      linkedin: { type: String, default: "" },
+      website: { type: String, default: "" },
+    },
+    experience: [
+      {
+        company: { type: String },
+        position: { type: String },
+        start_date: { type: String },
+        end_date: { type: String },
+        description: { type: String },
+        is_current: { type: Boolean },
+      },
+    ],
+    projects: [
+      {
+        name: { type: String },
+        type: { type: String },
+        description: { type: String },
+      },
+    ],
+    education: [
+      {
+        institution: { type: String },
+        degree: { type: String },
+        field: { type: String },
+        graduation_date: { type: String },
+        gpa: { type: String },
+      },
+    ],
+    }
+    `;
+    const response = await ai.chat.completions.create({
+      model: process.env.OPENAI_MODEL,
+      messages: [
+        {
+          role: "system",
+          content: systemPrompt,
+        },
+        {
+          role: "user",
+          content: userPrompt,
+        },
+      ],
+      response_format: { type: "json_object" },
+    });
+    const extractedData = response.choices[0].message.content;
+    const parsedData = JSON.parse(extractedData);
+    const newResumse = await Resume.create({ userId, title, ...parsedData });
+    return res.status(200).json({ resumeId: newResumse._id });
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }

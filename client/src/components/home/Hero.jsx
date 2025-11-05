1d33ffe7efc23fbdbf8f26d5eb9e4cc090828025
Shivansh Nigam <s2704nigam@gmail.com>
2025-11-05 19:44:30 +0530

    Almost Final

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -19,10 +19,6 @@ const Hero = () => {
         <div className="min-h-screen pb-20">
           {/* Navbar */}
           <nav className="z-50 flex items-center justify-between w-full py-4 px-6 md:px-16 lg:px-24 xl:px-40 text-sm">
-            <a href="https://prebuiltui.com">
-              <img src="/logo.svg" alt="logo" className="h-11 w-auto" />
-            </a>
-
             <div className="hidden md:flex items-center gap-8 transition duration-500 text-slate-800">
               <a href="#" className="hover:text-green-600 transition">
                 Home
@@ -187,44 +183,12 @@ const Hero = () => {
             {/* CTA Buttons */}
             <div className="flex items-center gap-4 ">
               <Link
-                to="/app"
+                to="/app?state=register"
                 className="bg-green-500 hover:bg-green-600 text-white rounded-full px-9 h-12 m-1 ring-offset-2 ring-1 ring-green-400 flex items-center transition-colors"
               >
                 Get started
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="lucide lucide-arrow-right ml-1 size-4"
-                  aria-hidden="true"
-                >
-                  <path d="M5 12h14"></path>
-                  <path d="m12 5 7 7-7 7"></path>
-                </svg>
               </Link>
               <button className="flex items-center gap-2 border border-slate-400 hover:bg-green-100 transition rounded-full px-7 h-12 text-slate-700">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="1"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="lucide lucide-video size-5"
-                  aria-hidden="true"
-                >
-                  <path d="m16 13 5.223 3.482a.5.5 0 0 0 .777-.416V7.87a.5.5 0 0 0-.752-.432L16 10.5"></path>
-                  <rect x="2" y="6" width="14" height="12" rx="2"></rect>
-                </svg>
                 <span>Try demo</span>
               </button>
             </div>
